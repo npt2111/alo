3e2f399becac21e26c11527aa388c99eb0979bee
thuan2104 <127218203+npt2111@users.noreply.github.com>
2024-12-09 21:30:23 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -34,9 +34,3 @@ function minimizeNotification(button) {
   notification.style.display = 'none'; 
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  var encodedText = '&#68;&#101;&#115;&#105;&#103;&#110;&#32;&#98;&#121;&#32;&#80;&#97;&#110;&#98;&#97;&#112;';
-  var footer = document.createElement("a");
-  footer.innerHTML = encodedText; 
-  document.body.appendChild(footer); 
-});
